c93795878cb27bdc7278603079823108d8e382b2
Redume <danilivasenko09@gmail.com>
2022-07-12 12:11:17 +0300

    Добавление тостов

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from win10toast import ToastNotifier
+
 from bs4 import BeautifulSoup
 from elevate import elevate
 import requests
@@ -16,9 +18,9 @@ class Nasa:
         self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) '
                                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
         self.photoName = "everydayphotonasa.jpg"
+        self.toaster = ToastNotifier()
 
-    @staticmethod
-    def autorun():
+    def autorun(self):
         path = os.path.dirname(os.path.realpath(__file__))
         address = os.path.join(path, "main.exe")
         key_value = "Software/Microsoft/Windows/CurrentVersion/Run"
@@ -26,7 +28,10 @@ class Nasa:
         key = reg.OpenKey(reg.HKEY_LOCAL_MACHINE, key_value, 0, reg.KEY_ALL_ACCESS)
         reg.SetValueEx(key, user, 0, reg.REG_SZ, address)
         reg.CloseKey(key)
-        print("Программа добавлена в автозапуск")
+        self.toaster.show_toast("EveryDayPhotoNasa",
+                                "Программа добавлена в автозапуск.",
+                                duration=5,
+                                icon_path=None)
 
     def download_photo(self):
         try:
@@ -35,7 +40,6 @@ class Nasa:
             lnk = str
             for link in soup.select("img"):
                 lnk = link["src"]
-                print(f"Сохарняю картинку — {self.url + lnk}")
 
             img = urllib.request.urlopen(self.url + lnk).read()
             out = open(self.photoName, "wb")
@@ -43,20 +47,24 @@ class Nasa:
             out.close()
             self.set_wallpaper()
         except requests.exceptions.ConnectionError:
-            return print("Нет подключения к интернету")
+            return self.toaster.show_toast("EveryDayPhotoNasa",
+                                           "Не получилось подключится к сайту, проверьте подключение к интернету.",
+                                           duration=5,
+                                           icon_path=None)
 
     def set_wallpaper(self):
         path = os.path.abspath(self.photoName)
-        print("Установлено фоновое изображение")
         ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+        self.toaster.show_toast("EveryDayPhotoNasa",
+                                "Обои поставлены.",
+                                duration=5,
+                                icon_path=None)
 
     def start(self):
         self.download_photo()
-        print("Можно закрывать программу")
         if ctypes.windll.shell32.IsUserAnAdmin() != 0:
             elevate(show_console=False, graphical=False)
             self.autorun()
-            print("Программа добавлена в автозапуск, можете закрывать программу")
 
 
 if __name__ == "__main__":
