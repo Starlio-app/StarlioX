2e2b079fc474e80991f168773221d4b25f53138b
Redume <Danilivasenko09@gmail.com>
2022-09-19 02:06:28 +0300

    add error handler

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -11,5 +11,8 @@ func Message(status bool, message string) map[string]interface{} {
 
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		panic(err)
+	}
 }
