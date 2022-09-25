8409cd7b7587f64ba429fb9883701a14628c4192
Redume <Danilivasenko09@gmail.com>
2022-09-25 11:05:57 +0300

    added delete "v" in version

--- web/static/scripts/version.js
+++ web/static/scripts/version.js
@@ -2,6 +2,6 @@ $.ajax({
     url: "http://localhost:8080/api/get/version",
     type: "GET",
     success: function(data){
-        $("#program-version").text(data.message);
+        $("#program-version").text(data.message.replace("v", ""));
     }
 })
