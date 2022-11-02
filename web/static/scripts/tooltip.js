983d03575fd25d89550830c9b5d981cefe0be082
Redume <Danilivasenko09@gmail.com>
2022-11-02 20:53:33 +0300

    Delete web directory

--- web/static/scripts/tooltip.js
+++
@@ -1,2 +0,0 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
