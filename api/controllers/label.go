0fa390c8ee5df046a8ffc7b6b59c6e9ab71f849b
Danil Ivasenko <danilivasenko09@gmail.com>
2023-03-18 16:50:33 +0300

    change name folder, project file optimization

--- api/controllers/label.go
+++ api/controllers/label.go
@@ -5,27 +5,26 @@ import (
 	"os/user"
 	"strings"
 
-	"github.com/Redume/EveryNasa/api/utils"
-	"github.com/Redume/EveryNasa/functions"
+	"github.com/Redume/EveryNasa/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
 var CreateLabel = func(c *fiber.Ctx) error {
 	u, err := user.Current()
 	if err != nil {
-		functions.Logger(err.Error())
+		utils.Logger(err.Error())
 	}
 
 	dir, err := os.Getwd()
 	if err != nil {
-		functions.Logger(err.Error())
+		utils.Logger(err.Error())
 	}
 
 	dir = strings.Replace(dir, "\\", "\\\\", -1) + "\\EveryNasa.exe"
 
-	err = functions.CreateLnk(dir, strings.Replace(u.HomeDir, "\\", "\\\\", -1)+"\\Desktop\\EveryNasa.lnk")
+	err = utils.CreateLnk(dir, strings.Replace(u.HomeDir, "\\", "\\\\", -1)+"\\Desktop\\EveryNasa.lnk")
 	if err != nil {
-		functions.Logger(err.Error())
+		utils.Logger(err.Error())
 	}
 
 	utils.Respond(c, utils.Message(true, "The shortcut was successfully created"))
